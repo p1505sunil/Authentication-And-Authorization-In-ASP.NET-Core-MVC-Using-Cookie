71fcadff096c0c738dc8e03a574cdcf38806629b
Venkatesh <vkalva45@gmail.com>
2019-11-03 19:42:44 -0600

    modify the Login method  where we are providing two different kinds of roles; one is Admin role and another is User role. Based on these roles, we will provide access to some of the pages.

--- WebApplication3/Controllers/CoreNlog.cs
+++ WebApplication3/Controllers/CoreNlog.cs
@@ -1,42 +1,46 @@
-﻿using CookieDemo.Models;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-
-namespace CookieDemo.Controllers
+﻿[HttpPost]
+public IActionResult Login(string userName, string password)
 {
-
-    [Authorize]
-    public class HomeController : Controller
+    if (!string.IsNullOrEmpty(userName) && string.IsNullOrEmpty(password))
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        return RedirectToAction("Login");
     }
+
+    //Check the user name and password  
+    //Here can be implemented checking logic from the database  
+    ClaimsIdentity identity = null;
+    bool isAuthenticated = false;
+
+    if (userName == "Admin" && password == "password")
+    {
+
+        //Create the identity for the user  
+        identity = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, userName),
+                    new Claim(ClaimTypes.Role, "Admin")
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        isAuthenticated = true;
+    }
+
+    if (userName == "Mukesh" && password == "password")
+    {
+        //Create the identity for the user  
+        identity = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, userName),
+                    new Claim(ClaimTypes.Role, "User")
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        isAuthenticated = true;
+    }
+
+    if (isAuthenticated)
+    {
+        var principal = new ClaimsPrincipal(identity);
+
+        var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+        return RedirectToAction("Index", "Home");
+    }
+    return View();
 }
